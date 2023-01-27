5f0cc9cbdb5da02f86b6ee7bf9c0301d519c9a7f
wkddntjr1123 <wkddntjr1123@gmail.com>
2023-01-27 17:55:57 +0900

    feat: not using java Reflection

---
+++ src/main/java/com/cos/reflect/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.cos.reflect.controller;
+
+public class UserController {
+    public void join(){
+        System.out.println("called join()");
+    }
+
+    public void login(){
+        System.out.println("called login()");
+    }
+
+    public void user(){
+        System.out.println("called user()");
+    }
+}
--- src/main/java/com/cos/reflect/filter/Dispatcher.java
+++ src/main/java/com/cos/reflect/filter/Dispatcher.java
@@ -1,11 +1,30 @@
 package com.cos.reflect.filter;
 
+import com.cos.reflect.controller.UserController;
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class Dispatcher implements Filter {
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("Dispatcher Enter!");
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+//        System.out.println("context path : " + request.getContextPath());
+//        System.out.println("uri path : " + request.getRequestURI());
+//        System.out.println("full url : " + request.getRequestURL());
+
+        String endPoint = request.getRequestURI().replaceAll(request.getContextPath(),"");
+//        System.out.println(endPoint);
+
+        UserController userController = new UserController();
+        if(endPoint.equals("/join")){
+            userController.join();
+        } else if (endPoint.equals("/login")){
+            userController.login();
+        }
     }
 }
