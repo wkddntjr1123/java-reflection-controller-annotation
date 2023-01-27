a312d73db23244180c360171c285bbc26e114065
wkddntjr1123 <wkddntjr1123@gmail.com>
2023-01-27 21:07:13 +0900

    feat: Controller's method get Dto as single param

--- src/main/java/com/cos/reflect/controller/UserController.java
+++ src/main/java/com/cos/reflect/controller/UserController.java
@@ -1,21 +1,25 @@
 package com.cos.reflect.controller;
 
 import com.cos.reflect.annotation.RequestMapping;
+import com.cos.reflect.controller.dto.JoinDto;
+import com.cos.reflect.controller.dto.LoginDto;
 
 public class UserController {
 
     @RequestMapping("/join")
-    public String join(){
+    public String join(JoinDto dto) {
+        System.out.println(dto);
         return "/";
     }
 
     @RequestMapping("/login")
-    public String login(){
+    public String login(LoginDto dto) {
+        System.out.println(dto);
         return "/";
     }
 
     @RequestMapping("/user")
-    public String user(){
+    public String user() {
         return "/";
     }
 }
